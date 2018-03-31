23e7bb8fec80ae8c5ba08b8681bb5aba098229f8
Wheely <mittgfu@gmail.com>
2018-03-31 14:24:55 +0200

    Almost done with gamerules for Klondike

--- src/js/klondike.js
+++ src/js/klondike.js
@@ -39,12 +39,14 @@ SOL.clickCard = function (cardInfo) {
 
   // Clicking on deck
   if (cardInfo.stack === 0) {
+    console.log('Deck cycle');
     SOL.move(0, 1, 3, true, true);
     SOL.deselectLast();
 
   // Uncover face down card
   } else if (cardInfo.card.facedown) {
  
+    console.log('Uncover facedown');
     SOL.stats.updateScore(SOL.scoring.uncoverFaceDown);
     cardInfo.card.facedown = false;
     document.getElementById(cardInfo.card.id).classList.remove('facedown');
@@ -54,22 +56,32 @@ SOL.clickCard = function (cardInfo) {
   } else if (SOL.game.activeCard === null) {
     SOL.game.activeCard = cardInfo;
     SOL.highlight(cardInfo);
+    console.log('Only select card');
 
   // Deselect on same-card-twice or push to foundation
   } else if (SOL.game.activeCard.card === cardInfo.card) {
-    for (let j = 2; j < 6; j++) {
-      if (SOL.game.stacks[j].length === 0
-        && SOL.game.activeCard.card.value === 0) {
-        SOL.move(SOL.game.activeCard.stack, j, 1);
-        console.log(j);
-        break;
-      } else {
-        let lastCard = SOL.game.stacks[j][SOL.game.stacks[j].length - 1]
-        if (SOL.game.activeCard.card.value === lastCard.value + 1
-          && SOL.game.activeCard.card.color === lastCard.color){
+    // Moving ace to foundation
+    if (SOL.game.activeCard.card.value === 0) {
+      for (var j = 2; j < 6; j++) {
+        if (SOL.game.stacks[j].length === 0) {
           SOL.move(SOL.game.activeCard.stack, j, 1);
+          console.log('Move to empty foundation doubleclick');
+          break;
         }
-        break;
+      }
+    // Moving other card to foundation
+    } else {
+      for (var j = 2; j < 6; j++) {
+        if (SOL.game.stacks[j].length !== 0) {
+          var lastCard = SOL.game.stacks[j][SOL.game.stacks[j].length - 1]
+          console.log('lastcard: '+JSON.stringify(lastCard));
+          if (SOL.game.activeCard.card.value === lastCard.value + 1
+            && SOL.game.activeCard.card.color === lastCard.color){
+            console.log('Move to non-empty foundation double-click');
+            SOL.move(SOL.game.activeCard.stack, j, 1);
+            break;
+          }
+        }
       }
     }
     SOL.deselectLast();
@@ -82,14 +94,19 @@ SOL.clickCard = function (cardInfo) {
     if (SOL.game.activeCard.card.value === cardInfo.card.value - 1
       && SOL.differentColor(SOL.game.activeCard.card.color, cardInfo.card.color)
       && cardInfo.stack > 5) {
+      console.log('Normal move card');
       SOL.move(
         SOL.game.activeCard.stack,
         cardInfo.stack,
         SOL.game.stacks[SOL.game.activeCard.stack].length - SOL.game.activeCard.pos);
 
     // Wrong selection
+    } else if (SOL.game.activeCard.card.value === cardInfo.card.value + 1
+      && SOL.game.activeCard.card.color === cardInfo.card.color){
+      console.log('Move to non-empty foundation double-click');
+      SOL.move(SOL.game.activeCard.stack, cardInfo.stack, 1);
     } else {
-      console.log('Error flash');
+      console.log('Two-card interaction failure');
       // TODO Errorflash
     }
 
@@ -109,17 +126,19 @@ SOL.clickStack = function (stack) {
   if (SOL.game.activeCard
     && SOL.game.activeCard.card.value === 0
     && stack > 1 && stack < 6) {
+    console.log('Move card to foundation');
     SOL.move(SOL.game.activeCard.stack, stack, 1);
 
   // Move Kings to empty stack
   } else if (SOL.game.activeCard
     && SOL.game.activeCard.card.value === 12
     && stack > 5) {
+    console.log('Move king to empty field');
     SOL.move(SOL.game.activeCard.stack, stack, 1);
     // TODO MOVE ALL CARDS
   } else {
     // Error flash
-    console.log('Error flash');
+    console.log('Failed empty stack interaction');
   }
   setTimeout(function () {
       SOL.deselectLast();
